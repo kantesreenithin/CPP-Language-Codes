4f8a16745d32574d00894e154b6b057c6acf46d7
kantesreenithin <71339114+kantesreenithin@users.noreply.github.com>
2021-03-26 18:43:01 +0530

    Adding class and objects from HR

---
+++ class and objects HR.cpp
@@ -0,0 +1,55 @@
+//https://www.hackerrank.com/challenges/classes-objects/problem
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+// Write your Student class here
+class Student
+{   public:
+    int ar[5],sum=0;
+    void input()
+    {
+        for(int i=0;i<5;i++)
+        cin>>ar[i];
+    }
+    int calculateTotalScore()
+    {
+        for(int i=0;i<5;i++)
+        {
+            sum=sum+ar[i];
+        }
+        return sum;
+    }
+};
+
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    // print result
+    cout << count;
+    
+    return 0;
+}
+
